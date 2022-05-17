1e1490547efcc9dda7cb96304c9b33f492ab15f9
mfocqueteau <mfocqueteau@uc.cl>
2022-05-17 16:53:39 -0400

    feat(front): hay detalles que mejorar (i.e. links)

---
+++ src/components/JokeCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps({
+  joke: String,
+});
+</script>
+
+<template>
+  <div class="joke-card">
+    <p>
+      <strong>{{ joke || 'Aquí va el chiste' }}</strong>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.joke-card {
+  width: 40ch;
+  color: #333;
+  display: flex;
+  flex-direction: row;
+  padding: 1rem;
+  background-color: white;
+  border-radius: 4px;
+  border: solid 1px lightgray;
+  box-shadow: 2px 4px 6px rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="navbar">
+    <a href="/">Home</a>
+    <a href="/ihazdadjokes">Dad Jokes</a>
+    <a href="/foaas">F-Off</a>
+    <a href="/geek-jks">Geek</a>
+    <a href="/favourites">Favourites</a>
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  width: 100%;
+  height: 2rem;
+  display: flex;
+  background-color: black;
+}
+
+.navbar {
+  padding: 0;
+  height: 1.5em;
+  width: 100%;
+  color: white;
+  max-width: 100%;
+  max-height: 6rem;
+  min-height: 3rem;
+  background-color: black;
+}
+
+a {
+  width: 11ch;
+  height: 100%;
+  display: flex;
+  color: white;
+  padding: 0 0.125rem;
+  align-items: center;
+  text-decoration: none;
+  justify-content: center;
+}
+
+a:hover {
+  color: black;
+  background-color: aquamarine;
+}
+</style>
