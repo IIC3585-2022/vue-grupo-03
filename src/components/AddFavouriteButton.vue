0569721e4780b4ced577e07fe5776de3d5f006f5
mfocqueteau <mfocqueteau@uc.cl>
2022-05-21 16:57:09 -0400

    feat(state): first approach to state management

---
+++ src/components/AddFavouriteButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+const store = useCounterStore();
+</script>
+
+<template>
+  <button @click="store.increment()">Añadir a favoritos</button>
+</template>
+
+<style scoped>
+button {
+  background-color: #fe3;
+  transition: background-color 100ms linear;
+}
+
+button:active {
+  background-color: goldenrod;
+}
+</style>
